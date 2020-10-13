314a07520232293da415b32cf49387a674281c65
jeon-soyeong <jsu3417@naver.com>
2020-10-13 15:41:11 +0900

    부엌 채우기 scrollView로 수정, 메모 글자수 추가

--- KitchenDiary/FillInIngredientsController.swift
+++ KitchenDiary/FillInIngredientsController.swift
@@ -1,31 +1,26 @@
 //
-//  DiaryDetailController.swift
+//  FillInIngredientsController.swift
 //  KitchenDiary
 //
-//  Created by 전소영 on 2020/09/24.
+//  Created by 전소영 on 2020/10/13.
 //  Copyright © 2020 Soyeong Jeon. All rights reserved.
 //
-
 import UIKit
-import os.log
 
-class DiaryDetailController: UIViewController {
-    
-    @IBOutlet weak var memoHeight: NSLayoutConstraint!
-    @IBOutlet weak var viewHeight: NSLayoutConstraint!
-    @IBOutlet weak var memoText: UITextView!
-    @IBOutlet weak var saveButton: UIBarButtonItem!
-    @IBOutlet weak var photoImageView: UIImageView!
+class FillInIngredientsController: UIViewController {
+
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var memoText: UITextView!
     @IBOutlet weak var textCount: UILabel!
-    @IBOutlet weak var memoTextBottom: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.memoText.layer.borderWidth = 1.0
-        self.memoText.layer.borderColor = UIColor.black.cgColor
+        
         memoText.delegate = self
         scrollView.delegate = self
+        
+        memoText.layer.borderWidth = 1.0
+        memoText.layer.borderColor = UIColor.black.cgColor
         
         registerForKeyboardNotification()
         
@@ -34,37 +29,29 @@ class DiaryDetailController: UIViewController {
         singleTapGestureRecognizer.isEnabled = true
         singleTapGestureRecognizer.cancelsTouchesInView = false
         scrollView.addGestureRecognizer(singleTapGestureRecognizer)
-        photoImageView.isUserInteractionEnabled = true
         
         textViewDidChange(memoText)
     }
 }
 
-
-extension DiaryDetailController: UITextViewDelegate {
-
-    //tap시 keyborad 내리기
-    @objc func MyTapMethod(sender: UITapGestureRecognizer) {
-            self.view.endEditing(true)
-    }
-
+extension FillInIngredientsController: UITextViewDelegate {
+    
     func registerForKeyboardNotification(){
             NotificationCenter.default.addObserver(self, selector: #selector(keyBoardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
             NotificationCenter.default.addObserver(self, selector: #selector(keyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
-
-    @objc func keyBoardShow(notification: NSNotification){
+    
+    @objc func keyBoardShow(notification: NSNotification) {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue { UIView.animate(withDuration: 0.3, animations: { self.view.transform = CGAffineTransform(translationX: 0, y: -keyboardSize.height) }) }
        }
 
-    @objc func keyboardHide(_ notification: Notification){
+    @objc func keyboardHide(_ notification: Notification) {
         self.view.transform = .identity
     }
     
-    //memo 글자수 세기
-    func textViewDidChange(_ textView: UITextView) {
-        let memoCount = "\(textView.text.count)"
-        textCount.text = memoCount
+    //tap시 keyborad 내리기
+    @objc func MyTapMethod(sender: UITapGestureRecognizer) {
+            self.view.endEditing(true)
     }
     
     //memoText.frame.height이 0보다 작으면 backspace 안되게 하기
@@ -89,41 +76,17 @@ extension DiaryDetailController: UITextViewDelegate {
        // make sure the result is under 200 characters
        return updatedText.count <= 200
     }
+    
+    //memo 글자수 세기
+    func textViewDidChange(_ textView: UITextView) {
+        let memoCount = "\(textView.text.count)"
+        textCount.text = memoCount
+    }
 }
 
-extension DiaryDetailController: UIScrollViewDelegate {
+extension FillInIngredientsController: UIScrollViewDelegate {
     //scroll시 keyborad 내리기
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
             self.view.endEditing(true)
-    }
-}
-
-
-extension DiaryDetailController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-   
-    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
-            let imagePickerController = UIImagePickerController()
-            //image를 가져올 위치를 지정(enum type)
-            imagePickerController.sourceType = .photoLibrary
-            imagePickerController.delegate = self
-            present(imagePickerController, animated:  true, completion:  nil)
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        dismiss(animated: true, completion: nil)
-    }
-    
-   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-
-        // The info dictionary may contain multiple representations of the image. You want to use the original.
-        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
-            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
-        }
-
-        // Set photoImageView to display the selected image.
-        photoImageView.image = selectedImage
-
-        // Dismiss the picker.
-        dismiss(animated: true, completion: nil)
     }
 }
