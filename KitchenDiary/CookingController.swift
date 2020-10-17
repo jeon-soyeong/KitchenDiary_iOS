7775a3b9f5272e4d5f9002969ba75b099f6f6933
jeon-soyeong <jsu3417@naver.com>
2020-10-18 02:21:09 +0900

    [WIP] 기본재료에 맞는 레시피 데이터 parsing 작업중

---
+++ KitchenDiary/CookingController.swift
@@ -0,0 +1,30 @@
+//
+//  CookingController.swift
+//  KitchenDiary
+//
+//  Created by 전소영 on 2020/10/17.
+//  Copyright © 2020 Soyeong Jeon. All rights reserved.
+//
+
+import UIKit
+
+class CookingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
